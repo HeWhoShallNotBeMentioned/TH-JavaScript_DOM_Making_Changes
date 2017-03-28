bedf27d33dbd64997ac403caa1860e93422e8777
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-28 00:38:48 -0700

    create button that will hide the list

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const input = document.querySelector("input");
+const toggleList = document.getElementById('toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector("input.description");
 const p = document.querySelector("p.description");
-const button = document.querySelector("button");
+const button = document.querySelector("button.description");
+
+toggleList.addEventListener("click", () => {
+  if (listDiv.style.display == "none") {
+    listDiv.style.display = "block";
+    toggleList.textContent = "Hide List";
+  } else {
+    listDiv.style.display = "none";
+    toggleList.textContent = "Show List";
+  }
+
+});
 
 button.addEventListener("click", () => {
   p.textContent = input.value + ':';
