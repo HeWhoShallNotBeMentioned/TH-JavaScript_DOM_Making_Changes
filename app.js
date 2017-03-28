9da13d2f8a195d26ee664cc7cbc265166f9e6eb7
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-28 01:41:03 -0700

    add new line item to the list and clear text inputs when button pushed

--- app.js
+++ app.js
@@ -18,11 +18,15 @@ toggleList.addEventListener("click", () => {
 
 descriptionButton.addEventListener("click", () => {
   descriptionP.textContent = descriptionInput.value + ':';
+  descriptionInput.value = "";
 });
 
 descriptionP.title = "List Description";
 
 addItemButton.addEventListener('click', () => {
+  let ul = document.getElementsByTagName('ul')[0];
   let li = document.createElement('li');
   li.textContent = addItemInput.value;
+  ul.appendChild(li);
+  addItemInput.value = "";
 });
